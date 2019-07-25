d57ba1a387d1f8a87491dca244fe3823064adefe
tychang <tychang@Nexify.com.hk>
2019-07-25 15:58:11 +0800

    add sidebar

--- src/Components/hover-window/hover-window.tsx
+++ src/Components/hover-window/hover-window.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { useStyles } from "./style";
 
-const dummydata = [
+const dummylist1 = [
   { name: "flo", number: 1 },
   { name: "wer", number: 2 },
   { name: "whyjustgonnahappened", number: 3 },
@@ -14,11 +14,18 @@ export const HoverWindow: FC = () => {
 
   return (
     <>
-      <div className={classes.root}>
-        {dummydata.map(data => (
-          <li key={data.name}>{data.name}</li>
+      <ul className={classes.root}>
+        {dummylist1.map(list1 => (
+          <li key={list1.number}>
+            <div>{list1.name}</div>
+            <ul>
+              <li>tes1</li>
+              <li>tes2</li>
+              <li>tes3</li>
+            </ul>
+          </li>
         ))}
-      </div>
+      </ul>
     </>
   );
 };
--- src/Components/hover-window/style.ts
+++ src/Components/hover-window/style.ts
@@ -1,20 +1,71 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { fontFamily, positions } from "@material-ui/system";
 import { NONAME } from "dns";
 
 export const useStyles = makeStyles(
   createStyles({
     root: {
+      margin: 0,
+      padding: 0,
+      // border: "#ccc 1px solid",
       display: "flex",
       flexDirection: "column",
       flexWrap: "wrap",
-      color: "#FFFF33",
-      "&>li": {
-        listStyle: "none"
+      fontFamily: "'Open Sans',Arial,sans-serif",
+      fontSize: "14px",
+      "&  li": {
+        listStyle: "none",
+        position: "relative",
+        whiteSpace: "nowrap"
+        // borderRight: "#ccc 1px solid",
       },
       "&:hover": {
-        color: "#33FFFF"
+        color: "#000"
+      },
+      "& div": {
+        backgroundColor: "#fff",
+        color: "#333",
+        display: "flex",
+        padding: "0 20px",
+        textDecoration: "none",
+        lineHeight: "60px"
+      },
+      "& div:hover": {
+        backgroundColor: "#ef5c28",
+        color: "#fff"
+      },
+      "& div:hover>div": {
+        backgroundColor: "#ef5c28",
+        color: "#fff"
+      },
+      "& ul": {
+        margin: 0,
+        padding: 0,
+        display: "none",
+        border: "#ccc 1px solid",
+        position: "absolute",
+        zIndex: 99,
+        right: "-191px",
+        top: 0,
+        minWidth: "191px"
+      },
+      "& li:hover>ul": {
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        fontFamily: "'Open Sans',Arial,sans-serif",
+        backgroundColor: "#fff"
+      },
+      "& li:hover>ul>li:hover": {
+        backgroundColor: "#ef5c28"
+      },
+      "& ul li": {
+        minHeight: "60px",
+        borderBottom: "#ccc 1px solid"
+      },
+      "& ul li:last-child": {
+        borderBottom: "none"
       }
-    },
-    windows: {}
+    }
   })
 );
